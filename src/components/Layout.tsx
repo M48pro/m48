f5af45fbeac04e41b5727dfe7f3a91f2ba88d06f
M48pro <m48.office@gmail.com>
2025-06-04 20:22:19 +0300

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import ScrollToTop from '../utils/ScrollToTop';
+// src/components/Layout.tsx
+import React from 'react';
 
-const Layout: React.FC = () => {
-  const location = useLocation();
-  const [pageLoaded, setPageLoaded] = useState(false);
-  
-  useEffect(() => {
-    setPageLoaded(false);
-    
-    const timer = setTimeout(() => {
-      setPageLoaded(true);
-    }, 100);
-    
-    return () => clearTimeout(timer);
-  }, [location.pathname]);
-  
+const Layout: React.FC = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <ScrollToTop />
-      <Navbar />
-      <main className={`flex-grow transition-opacity duration-500 ${pageLoaded ? 'opacity-100' : 'opacity-0'}`}>
-        <Outlet />
-      </main>
-      <Footer />
+    <div>
+      <header>My App Header</header>
+      <main>{children}</main>
+      <footer>My App Footer</footer>
     </div>
   );
 };
