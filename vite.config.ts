01a450a5f6bab2266659dc3eadc7842514546a58
M48pro <m48.office@gmail.com>
2025-06-04 20:34:06 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@contexts': '/src/contexts',
+      '@routes': '/src/routes' // ← добавь это
+    }
+  }
+});
