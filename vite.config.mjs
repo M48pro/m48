ab220c2795427773ee10b39627d39716932b29c2
M48pro <m48.office@gmail.com>
2025-06-05 10:57:47 +0300

    Updated vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// Mimic __dirname in ES Module
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@routes': path.resolve(__dirname, './src/routes')
-    }
-  }
-});
+      '@': __dirname + '/src',
+    },
+  },
+})
